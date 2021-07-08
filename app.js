9d252a98dda1d49a95c08ced00355e11a84a4eaf
charLLLLLie <mega.joker00@gmail.com>
2021-07-08 19:55:12 +0300

    added player choice

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEAFULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function()
+{
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}`,'').toUpperCase();
+    if(selection !== ROCK && 
+    selection !== PAPER &&
+     selection !== SCISSORS 
+     ){
+        alert(`Invalid choice! WE choose ${ROCK} for you`)
+        return DEAFULT_USER_CHOICE;
+    }
+}
+
+startGameBtn.addEventListener('click', function(){
+    if(gameIsRunning){
+        return;
+    }
+    gameIsRunning = true;
+    console.log('Game is starting...');
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
+});
